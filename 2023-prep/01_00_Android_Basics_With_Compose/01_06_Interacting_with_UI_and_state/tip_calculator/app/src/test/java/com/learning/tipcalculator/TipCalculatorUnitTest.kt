44e622f8c311b55ab50553d9214872bc3469deac
Tirajiqta <72971042+Tirajiqta@users.noreply.github.com>
2023-10-31 23:29:55 +0200

    Finished tip calculator and art space project

--- 2023-prep/01_00_Android_Basics_With_Compose/01_06_Interacting_with_UI_and_state/tip_calculator/app/src/test/java/com/learning/tipcalculator/TipCalculatorUnitTest.kt
+++ 2023-prep/01_00_Android_Basics_With_Compose/01_06_Interacting_with_UI_and_state/tip_calculator/app/src/test/java/com/learning/tipcalculator/TipCalculatorUnitTest.kt
@@ -1,4 +1,17 @@
 package com.learning.tipcalculator
 
+import org.junit.Assert
+import org.junit.Test
+import java.text.NumberFormat
+
 class TipCalculatorUnitTest {
+
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        Assert.assertEquals(expectedTip, actualTip)
+    }
 }
