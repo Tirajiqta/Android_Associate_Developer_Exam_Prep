2378dd7ac27833454427be703167d83161df5cfd
Tirajiqta <72971042+Tirajiqta@users.noreply.github.com>
2023-11-02 22:53:21 +0200

    Finished The more kotlin fundamentals unit

--- 2023-prep/01_00_Android_Basics_With_Compose/01_07_More_kotlin_fundamentals/KotlinFundamentalsp2/src/main/kotlin/CollectionExamples.kt
+++ 2023-prep/01_00_Android_Basics_With_Compose/01_07_More_kotlin_fundamentals/KotlinFundamentalsp2/src/main/kotlin/CollectionExamples.kt
@@ -1,10 +1,48 @@
-fun main(){
+fun main() {
+    planetsAsArray()
+    planetsAsList()
+    planetsAsSet()
+    planetsAsMap()
+}
+
+fun planetsAsArray(): Unit {
 //    defining arrays in Kotlin
     val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
     val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
     val solarSystem = rockPlanets + gasPlanets
-    for(planet: String in solarSystem){
+    for (planet: String in solarSystem) {
 
         println(planet)
     }
+}
+
+fun planetsAsList(): Unit {
+//    lists in Kotlin
+    val planetsAsList = mutableListOf("Mercury", "Venus", "Earth", "Mars")
+    planetsAsList.add("Jupiter")
+    planetsAsList.remove("Mercury")
+
+}
+
+fun planetsAsSet(): Unit {
+    val solarSystem =
+        mutableSetOf("Mercury", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+}
+
+fun planetsAsMap(): Unit {
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystem.size)
+
+    solarSystem["Pluto"] = 5
+
 }
